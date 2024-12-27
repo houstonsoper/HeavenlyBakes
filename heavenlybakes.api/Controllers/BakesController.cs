11e9d2cec624eeb8bb4d4b11195d3a65a4007f43
houstonsoper <houstonsoper@gmail.com>
2024-12-27 14:04:59 +0000

    Initalized Web Api

---
+++ heavenlybakes.api/Controllers/BakesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace heavenlybakes.api.Controllers;
+
+[ApiController]
+[Route("bakes")]
+
+public class BakesController : ControllerBase
+{
+    public IActionResult Index()
+    {
+        return Ok();
+    }
+}
