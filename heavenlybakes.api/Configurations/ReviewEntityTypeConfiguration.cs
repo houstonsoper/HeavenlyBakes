1cf9f98a37ac6118dff1bf20dfd4cf319d8a8002
houstonsoper <houstonsoper@gmail.com>
2025-01-13 14:50:10 +0000

    Added a Review Model to API

---
+++ heavenlybakes.api/Configurations/ReviewEntityTypeConfiguration.cs
@@ -0,0 +1,8 @@
+﻿using heavenlybakes.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace heavenlybakes.api.Configurations;
+
+public class ReviewEntityTypeConfiguration : IEntityTypeConfiguration<Review>
+{
+}
---
+++ heavenlybakes.api/Models/Review.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using heavenlybakes.api.Enums;
+
+namespace heavenlybakes.api.Models;
+
+public class Review
+{
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "CustomerId is required.")]
+    public required string CustomerId { get; set; }
+    
+    [Required(ErrorMessage = "BakeId is required.")]
+    public required int BakeId { get; set; }
+    
+    [Required(ErrorMessage = "Title is required.")]
+    [MaxLength(100, ErrorMessage = "Maximum length is 50 characters.")]
+    public required string Title { get; set; } 
+    
+    [Required(ErrorMessage = "Feedback is required.")]
+    [MaxLength(500, ErrorMessage = "Maximum length is 500 characters.")]
+    public required string Feedback { get; set; } 
+
+    [Required(ErrorMessage = "Rating is required.")]
+    [Range(1, 5)]
+    public required int Rating { get; set; }
+    
+    public DateTime CreateDateTime { get; set; } = DateTime.Now;
+    
+    //Navigation
+    public Bake? Bake { get; set; } 
+}
