941e56d072d25a5651036a5ee5937bb64f647ea4
houstonsoper <houstonsoper@gmail.com>
2025-01-13 14:52:15 +0000

    Added ReviewEntityTypeConfiguration.cs

--- heavenlybakes.api/Configurations/ReviewEntityTypeConfiguration.cs
+++ heavenlybakes.api/Configurations/ReviewEntityTypeConfiguration.cs
@@ -1,8 +1,17 @@
 ﻿using heavenlybakes.api.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace heavenlybakes.api.Configurations;
 
 public class ReviewEntityTypeConfiguration : IEntityTypeConfiguration<Review>
+
 {
+    public void Configure(EntityTypeBuilder<Review> builder)
+    {
+        builder.HasOne(r => r.Bake)
+            .WithMany()
+            .HasForeignKey(r => r.BakeId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
 }
