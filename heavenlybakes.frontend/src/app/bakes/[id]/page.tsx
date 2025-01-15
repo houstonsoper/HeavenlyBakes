6244cb35b2384ac3e841bb86d8a55fe541e61092
houstonsoper <houstonsoper@gmail.com>
2025-01-15 16:57:40 +0000

    Added progress bars to the review section to display how many reviews for each rating

--- heavenlybakes.frontend/src/app/bakes/[id]/page.tsx
+++ heavenlybakes.frontend/src/app/bakes/[id]/page.tsx
@@ -18,34 +18,35 @@ import Stars from "@/components/stars";
 
 export default function Details() {
     const [bake, setBake] = useState<Bake | null>(null);
-    const params : Params = useParams();
-    const router : AppRouterInstance = useRouter();
+    const params: Params = useParams();
+    const router: AppRouterInstance = useRouter();
     const [quantity, setQuantity] = useState<number>(1);
     const {addToBasket} = useBasket();
     const [reviews, setReviews] = useState<Review[]>([]);
     const [rating, setRating] = useState<number>(0);
+    const stars: number[] = [1, 2, 3, 4, 5];
 
     useEffect(() => {
         const controller = new AbortController();
-        const signal : AbortSignal = controller.signal;
-        
+        const signal: AbortSignal = controller.signal;
+
         //Fetch the bake and it's details
         const getBakeDetails = async () => {
-            const fetchedBake : Bake | null = await fetchBakeById(Number(params.id), signal);
+            const fetchedBake: Bake | null = await fetchBakeById(Number(params.id), signal);
             if (fetchedBake) {
                 setBake(fetchedBake);
-                
+
                 //Fetch reviews
                 const fetchedReviews: Review[] = await fetchReviews({bakeId: fetchedBake.id}, signal);
                 if (fetchedReviews) setReviews(fetchedReviews)
-                
+
                 //Fetch rating
-                const rating : number = await fetchRating(fetchedBake.id, signal);
+                const rating: number = await fetchRating(fetchedBake.id, signal);
                 if (rating) setRating(rating);
             }
         }
         getBakeDetails();
-        
+
         return () => controller.abort();
     }, [params.id])
 
@@ -65,8 +66,8 @@ export default function Details() {
             router.push(`/basket`);
         }
     }
-    
-    console.log("reviews",reviews);
+
+    console.log("reviews", reviews);
 
     if (!bake) return null;
 
@@ -87,7 +88,7 @@ export default function Details() {
                         <div className="bake-details-container">
                             <h1 className="text-3xl font-bold text-pink-600">{bake.name}</h1>
                             <div className="flex mb-2">
-                                <Stars rating={rating} />
+                                <Stars rating={rating}/>
                                 <p className="ms-1 text-gray-400">{reviews.length}</p>
                             </div>
                             <div>
@@ -131,8 +132,26 @@ export default function Details() {
                         <div className="text-center">
                             <h1 className="pt-12">Customer reviews</h1>
                             <div className="inline-flex">
-                                <Stars rating={rating} />
+                                <Stars rating={rating}/>
                                 <p className="px-1 my-auto">{rating} out of 5</p>
+                            </div>
+                            {/* Progress Bars for each rating */}
+                            <div className="flex flex-col m-auto justify-center w-1/2">
+                                {stars.map((star: number) => {
+                                    const count : number = reviews.filter((r : Review) :boolean => r.rating === star).length; // Calculate the count for each star
+                                    return (
+                                        <div key={star} className="inline-flex text-nowrap">
+                                            <h3 className="my-auto pe-2">{star} Stars</h3>
+                                            <progress
+                                                className="my-2 w-full"
+                                                value={count}
+                                                max={reviews.length}
+                                                aria-label={`${count} reviews with ${star} stars`}
+                                            ></progress>
+                                            <h3 className="my-auto ps-2">({count})</h3>
+                                        </div>
+                                    );
+                                })}
                             </div>
                         </div>
                         <div className="w-3/4 m-auto">
