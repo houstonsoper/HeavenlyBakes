fb5baaece540b36b3be8cd82084e8b78c79c0e62
houstonsoper <houstonsoper@gmail.com>
2025-01-19 17:52:09 +0000

    Altered the return for bakes\page.tsx to return a "No results" message when search term doesn't match to any bakes

--- heavenlybakes.frontend/src/app/bakes/page.tsx
+++ heavenlybakes.frontend/src/app/bakes/page.tsx
@@ -1,7 +1,7 @@
 ﻿"use client"
 
 import {fetchBakes, fetchPopularBakes} from "@/services/bakeService";
-import {useEffect, useState} from "react";
+import {RefObject, useEffect, useRef, useState} from "react";
 import Bake from "@/interfaces/bake";
 import BakeCard from "../../components/bakeCard";
 import {useSearchParams} from "next/navigation";
@@ -12,8 +12,7 @@ export default function Page() {
     const searchParams = useSearchParams();
     const [filter, setFilter] = useState("");
     const [bakeType, setBakeType] = useState<string>("");
-    
-    console.log(searchParams.get("type"));
+    const searchTermRef : RefObject<string | null> = useRef<string | null>(null);
     
     //Fetch Bakes
     useEffect(() => {
@@ -22,75 +21,82 @@ export default function Page() {
             //Get the searchParam for search term and type
             const searchTerm: string | null = searchParams.get("search");
             const type: string | null = searchParams.get("type");
-            
+
             //Fetch bakes by search term/type if included, else fetch all
             const fetchedBakes: Bake[] = await fetchBakes({
-                searchTerm: searchTerm || undefined, 
+                searchTerm: searchTerm || undefined,
                 type: type || undefined
             });
             setBakes(fetchedBakes);
             type ? setBakeType(type) : setBakeType("All");
-            
+            searchTermRef.current = searchTerm;
+
         }
         getBakes();
     }, [searchParams])
-    
-    const handleFilter = (event : React.ChangeEvent<HTMLSelectElement>) => {
-            switch(event.target.value) {
-                //Most Popular
-                case "a":
-                    bakes.sort((a, b) => a.rating - b.rating);
-                    break;
-                //Price: Low to High    
-                case "b":
-                    bakes.sort((a, b) => a.price - b.price);
-                    break;
-                //Price: High to Low    
-                case "c":
-                    bakes.sort((a, b) => b.price - a.price);
-                    break;
-                //Name: A to Z  
-                case "d":
-                    bakes.sort((a, b) => a.name.localeCompare(b.name));
-                    break;
-                //Name: Z to A   
-                case "e":
-                    bakes.sort((a, b) => b.name.localeCompare(a.name));
-                    break;
-                //Discount 
-                case "f":
-                    bakes.sort((a, b) => b.discount - a.discount);
-                    break;
-                default:
-                    break;
-            }
-            setFilter(event.target.value);
+
+    const handleFilter = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        switch (event.target.value) {
+            //Most Popular
+            case "a":
+                bakes.sort((a, b) => a.rating - b.rating);
+                break;
+            //Price: Low to High    
+            case "b":
+                bakes.sort((a, b) => a.price - b.price);
+                break;
+            //Price: High to Low    
+            case "c":
+                bakes.sort((a, b) => b.price - a.price);
+                break;
+            //Name: A to Z  
+            case "d":
+                bakes.sort((a, b) => a.name.localeCompare(b.name));
+                break;
+            //Name: Z to A   
+            case "e":
+                bakes.sort((a, b) => b.name.localeCompare(a.name));
+                break;
+            //Discount 
+            case "f":
+                bakes.sort((a, b) => b.discount - a.discount);
+                break;
+            default:
+                break;
+        }
+        setFilter(event.target.value);
     }
-    
+
     return (
         <main>
             <div>
                 <PageHeader title="Our Selections" description="Indulge in our wide array of delectable treats. From classic cakes to mouthwatering pastries,
-                            we have something to satisfy every sweet tooth." />
-                
+                            we have something to satisfy every sweet tooth."/>
                 <section className="py-10">
-                    <div className="container mx-auto px-4 ">
-                        <h1 className="text-center">{bakeType}</h1>
-                        <div className="py-3">
-                            <select onChange={handleFilter} defaultValue="a">
-                                <option value="a">Most Popular</option>
-                                <option value="b">Price (Low To High)</option>
-                                <option value="c">Price (High to Low)</option>
-                                <option value="d">Name (A To Z) </option>
-                                <option value="e">Name (Z To A) </option>
-                                <option value="f">Discount</option>
-                            </select>
+                    {bakes.length > 0 ? (
+                        <div className="container mx-auto px-4 ">
+                            <h1 className="text-center">{bakeType}</h1>
+                            <div className="py-3">
+                                <select onChange={handleFilter} defaultValue="a">
+                                    <option value="a">Most Popular</option>
+                                    <option value="b">Price (Low To High)</option>
+                                    <option value="c">Price (High to Low)</option>
+                                    <option value="d">Name (A To Z)</option>
+                                    <option value="e">Name (Z To A)</option>
+                                    <option value="f">Discount</option>
+                                </select>
+                            </div>
+                            <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-12">
+                                {bakes.map((bake: Bake) => (<BakeCard key={bake.id} bake={bake}/>
+                                ))}
+                            </div>
                         </div>
-                        <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-y-12">
-                            {bakes.map((bake: Bake) => (<BakeCard key={bake.id} bake={bake}/>
-                            ))}
+                    ) : (
+                        <div className="text-center">
+                            <h1>No results for {searchTermRef.current}</h1>
+                            <p>Try checking your spelling or use more general terms</p>
                         </div>
-                    </div>
+                    )}
                 </section>
             </div>
         </main>
