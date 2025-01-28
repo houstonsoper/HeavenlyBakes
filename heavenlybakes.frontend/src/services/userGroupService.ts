3e6aa64dd700e041bbdcddeba1aa0ce736397059
houstonsoper <houstonsoper@gmail.com>
2025-01-28 15:12:26 +0000

    Added fetchAllUserGroups function

---
+++ heavenlybakes.frontend/src/services/userGroupService.ts
@@ -0,0 +1,17 @@
+﻿import UserGroup from "@/interfaces/userGroup";
+
+const BASEURL = "https://localhost:44367";
+export async function fetchAllUserGroups(signal? : AbortSignal) : Promise<UserGroup[] | []> {
+    try{
+        const url = `${BASEURL}/UserGroups`;
+        const response : Response = await fetch(url, {signal});
+        
+        if (!response.ok) {
+            throw new Error("Unable to fetch user groups");
+        }
+        
+        return await response.json();
+    } catch(error) {
+        throw new Error("Unable to fetch user groups");
+    }
+}
