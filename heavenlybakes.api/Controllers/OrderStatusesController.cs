b95458a247ac8d06b78cd63042e9282ab9cc237c
houstonsoper <houstonsoper@gmail.com>
2025-01-31 00:30:06 +0000

    Added endpoint to get all order statuses

---
+++ heavenlybakes.api/Controllers/OrderStatusesController.cs
@@ -0,0 +1,24 @@
+﻿using heavenlybakes.api.Models;
+using heavenlybakes.api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace heavenlybakes.api.Controllers;
+[ApiController]
+[Route("[controller]")]
+
+public class OrderStatusesController : Controller
+{
+    private readonly IOrderStatusesRepository _orderStatusesRepository;
+
+    public OrderStatusesController(IOrderStatusesRepository orderStatusesRepository)
+    {
+        _orderStatusesRepository = orderStatusesRepository;
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetOrderStatusesAsync()
+    {
+        var orderStatuses =  await _orderStatusesRepository.GetOrderStatusesAsync();
+        return Ok(orderStatuses);
+    }
+}
---
+++ heavenlybakes.api/Repositories/OrderStatusesRepository.cs
@@ -0,0 +1,20 @@
+﻿using heavenlybakes.api.Contexts;
+using heavenlybakes.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace heavenlybakes.api.Repositories;
+
+public class OrderStatusesRepository : IOrderStatusesRepository
+{
+    private readonly HeavenlyBakesDbContext _context;
+
+    public OrderStatusesRepository(HeavenlyBakesDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<IEnumerable<OrderStatus>> GetOrderStatusesAsync()
+    {
+       return await _context.OrderStatus.ToListAsync();
+    }
+}
