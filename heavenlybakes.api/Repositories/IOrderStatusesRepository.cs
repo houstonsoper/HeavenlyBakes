2ed51504208cbdbb48d00ac9e218852a99b6d1a9
houstonsoper <houstonsoper@gmail.com>
2025-01-31 00:32:16 +0000

    Added endpoint to get all order statuses

---
+++ heavenlybakes.api/Repositories/IOrderStatusesRepository.cs
@@ -0,0 +1,8 @@
+﻿using heavenlybakes.api.Models;
+
+namespace heavenlybakes.api.Repositories;
+
+public interface IOrderStatusesRepository
+{
+    Task<IEnumerable<OrderStatus>> GetOrderStatusesAsync();
+}
